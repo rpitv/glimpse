2c83a51ac72a0aabf2975a00f796c532f1f3e7c8
Erik Roberts <erik@bugg.co>
2019-12-14 14:49:45 -0500

    Added Header & a few temporary pages

---
+++ packages/glimpse-ui/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <ElMenu mode="horizontal" router="true" background-color="#8c3c3c" text-color="#ffffff" active-text-color="#ffdddd">
+    <ElMenuItem :router="this.$router" index="/">Home</ElMenuItem>
+    <ElMenuItem :router="this.$router" index="/productions">Productions</ElMenuItem>
+    <ElMenuItem :router="this.$router" index="/about">About</ElMenuItem>
+    <ElMenuItem :router="this.$router" index="/request">Request Service</ElMenuItem>
+    <NuxtLink to="/"><img src="../assets/rpitv_logo.png" alt="Logo" class="tv-logo" /></NuxtLink>
+  </ElMenu>
+</template>
+
+<script lang="ts">
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .header-wrapper {
+    background-color: #7c4e4e;
+    color: white;
+    padding: 20px;
+    position: fixed;
+    width: 100%;
+  }
+
+  .tv-logo {
+    position: absolute;
+    width: 200px;
+    left: calc(50% - 100px);
+    top: -20px;
+  }
+</style>
--- packages/glimpse-ui/layouts/default.vue
+++ packages/glimpse-ui/layouts/default.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
+    <Header />
     <nuxt />
   </div>
 </template>
 
-<style>
+<script lang="ts">
+import Header from '../components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Ubuntu|Oswald&display=swap');
+
+h1,h2,h3,h4,h5,h6 {
+  font-family: 'Oswald', sans-serif;
+}
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Ubuntu', sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -22,34 +37,5 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
