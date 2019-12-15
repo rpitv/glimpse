67b1f2e16f1602f36bb60ce839cc5ad88abc75de
Erik Roberts <erik@bugg.co>
2019-12-14 19:18:08 -0500

    updated header

--- components/Header.vue
+++ components/Header.vue
@@ -1,41 +1,78 @@
 <template>
   <div>
-    <VNavigationDrawer v-model="sidebar" app>
-      <VList>
+    <!-- Mobile Menu -->
+    <VNavigationDrawer v-model="sidebar" app temporary>
+      <VList nav>
         <VListItem class="drawer-header">
           <img src="../assets/rpitv_logo.png" alt="Logo" class="tv-logo">
           <h3>RPI TV</h3>
         </VListItem>
+
         <VDivider />
-        <VListItemTitle
-          v-for="item in menuItems"
+
+        <VListItem
+          v-for="item in leftMenuItems"
           :key="item.title"
           :to="item.path"
+          class="drawer-item"
         >
           <VListItemAction>
-            <font-awesome-icon :icon="['fad', item.icon]" />
+            <font-awesome-icon :icon="['fad', item.icon]" class="drawer-icon" />
           </VListItemAction>
-          <VListItemContent>{{ item.title }}</VListItemContent>
-        </VListItemTitle>
-      </VList>
-    </VNavigationDrawer>
-    <v-app-bar app>
-      <span class="hidden-sm-and-up">
-        <p @click="sidebar = !sidebar">
-          OPEN MENU
-        </p>
-      </span>
-      <VToolbarTitle class="app-nav-header">
-        <NuxtLink to="/">
-          <img src="../assets/rpitv_logo.png" alt="Logo" class="tv-logo">
-        </NuxtLink>
-      </VToolbarTitle>
-      <VToolbarItems class="hidden-xs-only">
-        <VBtn
-          v-for="item in menuItems"
+
+          <VListItemContent class="drawer-item-content">
+            {{ item.title }}
+          </VListItemContent>
+        </VListItem>
+
+        <VDivider />
+
+        <VListItem
+          v-for="item in rightMenuItems"
           :key="item.title"
           :to="item.path"
-          flat
+          class="drawer-item"
+        >
+          <VListItemAction>
+            <font-awesome-icon :icon="['fad', item.icon]" class="drawer-icon" />
+          </VListItemAction>
+
+          <VListItemContent class="drawer-item-content">
+            {{ item.title }}
+          </VListItemContent>
+        </VListItem>
+      </VList>
+    </VNavigationDrawer>
+
+    <v-app-bar app elevate-on-scroll>
+      <span class="hidden-sm-and-up">
+        <font-awesome-icon :icon="['fal', 'bars']" @click="sidebar = !sidebar" class="drawer-icon" />
+      </span>
+
+      <VToolbarTitle class="app-nav-header">
+        <img @load="fadeInLogo" src="../assets/rpitv_logo.png" alt="Logo" class="tv-logo">
+      </VToolbarTitle>
+
+      <!-- Left menu items -->
+      <VToolbarItems class="hidden-xs-only">
+        <VBtn
+          v-for="item in leftMenuItems"
+          :key="item.title"
+          :to="item.path"
+          :nuxt="true"
+          text
+        >
+          {{ item.title }}
+        </VBtn>
+      </VToolbarItems>
+      <!-- Right menu items -->
+      <VToolbarItems class="hidden-xs-only app-nav-right-items">
+        <VBtn
+          v-for="item in rightMenuItems"
+          :key="item.title"
+          :to="item.path"
+          :nuxt="true"
+          text
         >
           {{ item.title }}
         </VBtn>
@@ -48,46 +85,69 @@
 export default {
   data (): any {
     return {
-      appTitle: 'Awesome App',
       sidebar: false,
-      menuItems: [
+      leftMenuItems: [
         { title: 'Home', path: '/', icon: 'house' },
         { title: 'Productions', path: '/productions', icon: 'camera-movie' },
         { title: 'Request Us', path: '/request', icon: 'file-signature' },
         { title: 'About', path: '/about', icon: 'info-circle' }
+      ],
+      rightMenuItems: [
+        { title: 'Login', path: '/login', icon: 'sign-in' }
       ]
+    }
+  },
+  methods: {
+    fadeInLogo (e: any) {
+      e.target.classList.toggle('visible')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .header-wrapper {
-    background-color: #7c4e4e;
-    color: white;
-    padding: 20px;
-    position: fixed;
-    width: 100%;
+  .app-nav-right-items {
+    position: absolute;
+    right: 15px;
+  }
+
+  .drawer-item {
+    padding-left: 15px;
+    .drawer-icon {
+      --fa-primary-color: #b30000;
+      --fa-secondary-color: #9c0000;
+    }
+    .drawer-item-content {
+      display: inline;
+    }
   }
   .drawer-header .tv-logo {
-    width: 50px;
+    width: 75px;
     margin-right: 10px;
   }
 
   .app-nav-header .tv-logo {
-    @media(max-device-width: 600px) {
+    &.visible {
+      opacity: 1;
+    }
+    opacity: 0;
+
+    transition: opacity 0.5s;
+    @media(min-width: 600px) and (max-width: 700px) {
       display: none;
     }
-    @media(max-device-width: 1049px) {
+    @media (max-width: 1049px) {
       height: 60px;
       float: left;
       margin-right: 15px;
     }
-    @media(min-device-width: 1050px) {
+    @media (min-width: 1050px), (max-width: 600px) {
+      pointer-events: none;
       position: absolute;
       height: 150px;
       left: calc(50% - 100px);
       top: -20px;
+      z-index: 3000;
     }
   }
 </style>
