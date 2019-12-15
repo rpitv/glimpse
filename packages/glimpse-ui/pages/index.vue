f22d0f3951ebacbfc36a0db4e15d4a0e822aecb5
Erik Roberts <erik@bugg.co>
2019-12-15 01:51:43 -0500

    Experimenting with layout ideas Fixed issue with some files being ignored

--- packages/glimpse-ui/pages/index.vue
+++ packages/glimpse-ui/pages/index.vue
@@ -1,6 +1,45 @@
 <template>
   <div class="container">
-
+    <VRow>
+      <VCol sm="8">
+        <VSkeletonLoader type="image,article" height="400" class="intro-image-lg" />
+      </VCol>
+      <VCol sm="4">
+        <VSkeletonLoader type="image" class="intro-image-sm" />
+        <VSkeletonLoader type="image" class="intro-image-sm" />
+      </VCol>
+    </VRow>
+    <VRow>
+      <VCol lg="6">
+        <h1>Catch our next livestream:</h1>
+        <div class="player-wrapper">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/kMyrghxpwy4"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+        </div>
+      </VCol>
+      <VCol lg="6">
+        <h1>We are RPI TV.</h1>
+        <p>
+          RPI TV is a student-run organization at Rensselaer Polytechnic Institute dedicated to providing
+          the best television broadcasts of RPI events.
+        </p>
+        <p>
+          Interested in joining the club? We meet every Tuesday at 9pm in the Rensselaer Union, room 3202. If
+          this doesn't work for you, get in contact with us! <NuxtLink to="/join">
+            More information can be found here.
+          </NuxtLink>
+        </p>
+      </VCol>
+    </VRow>
+    <h2>Recent Productions</h2>
+    <div class="past-prod-list">
+      <VSkeletonLoader v-for="n in 10" :index="n" type="card, list-item-two-line" class="past-prod-card" />
+    </div>
   </div>
 </template>
 
@@ -11,7 +50,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .container {
-    min-height: 200vh
+  .player-wrapper iframe {
+    border: 0;
+  }
+  .intro-image-sm {
+    height: 165px;
+  }
+
+  .past-prod-list {
+    white-space: nowrap;
+    overflow-x: scroll;
+    .past-prod-card {
+      display: inline-block;
+      width: 300px;
+      margin-right: 20px;
+    }
   }
 </style>
