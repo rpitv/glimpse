031948f01b1da841c8ed1eaca9ce65abade3b6a2
Erik Roberts <erik@bugg.co>
2019-12-14 20:24:09 -0500

    Added social media & more links Updated footer

--- packages/glimpse-ui/components/Footer.vue
+++ packages/glimpse-ui/components/Footer.vue
@@ -1,17 +1,28 @@
 <template>
   <div>
-    <VFooter absolute app>
-      <p>Copyright rpitv</p>
+    <VFooter absolute app class="app-footer">
+      <p>All content © 2003–{{ year }} RPI TV. | All Rights Reserved.</p>
+      <p>
+        <NuxtLink to="/privacy">
+          Privacy Policy
+        </NuxtLink>
+      </p>
     </VFooter>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-
+  data (): any {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  p {
+    margin-right: 10px;
+  }
 </style>
