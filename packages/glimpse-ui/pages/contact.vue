8f739785b584c8c707e5801a1b30c776ba9604fd
Erik Roberts <erik@bugg.co>
2019-12-17 18:58:33 -0500

    Added custom titles and descriptions to pages

--- packages/glimpse-ui/pages/contact.vue
+++ packages/glimpse-ui/pages/contact.vue
@@ -15,6 +15,19 @@ export default {
   components: { ContactCard },
   data () {
     return {
+      title: 'Contact us / Request our services - RPI TV',
+      description: 'Fill out this form if you have any questions, feedback, or would like to request our services for a campus event.'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'og:title', name: 'og:title', content: this.title },
+        { hid: 'og:description', name: 'og:description', content: this.description },
+        { hid: 'description', name: 'description', content: this.description },
+        { hid: 'twitter:description', name: 'twitter:description', content: this.description }
+      ]
     }
   }
 }
