6bde1604a34a27973d58b3a1e99875b37336dd16
bugfroggy <erik@bugg.co>
2020-04-11 02:31:23 -0400

    Finished RecentProductionsList.vue data fetching Added VideoCard.vue Added sample data to NextLivestream.vue and VideoCard.vue for demonstration purposes and proof-of-concept Fixed .gitignore typo Added proxy for static content served by server

--- packages/glimpse-ui/components/RecentProductionsList.vue
+++ packages/glimpse-ui/components/RecentProductionsList.vue
@@ -1,31 +1,71 @@
 <template>
   <div class="past-prod-list">
-    <VSkeletonLoader
-      v-for="n in 10"
-      :index="n"
-      :key="n"
-      type="card, list-item-two-line"
+    <VCard
+      :href="'/productions/' + prod.id"
+      v-for="prod in productions"
+      :index="prod.id"
+      :key="prod.id"
       class="past-prod-card"
       boilerplate
-    />
+    >
+      <VImg :src="prod.thumbnail.link" :title="prod.thumbnail.name" height="200px" />
+      <VCardTitle>{{ prod.name }}</VCardTitle>
+      <VCardSubtitle>{{ prod.description }}</VCardSubtitle>
+      <VRow>
+        <VCol>
+          <VCardText class="v-card__subtitle">
+            {{ getFormattedDate(prod) }}
+          </VCardText>
+        </VCol>
+        <VCol>
+          <VCardActions class="prod-actions">
+            <VBtn :href="'/productions/' + prod.id" text color="primary">
+              Watch
+            </VBtn>
+          </VCardActions>
+        </VCol>
+      </VRow>
+    </VCard>
   </div>
 </template>
 
 <script>
 import gql from 'graphql-tag'
+import moment from 'moment'
 
 export default {
   name: 'RecentProductionsList',
+  data () {
+    return {
+      productions: []
+    }
+  },
+  methods: {
+    getFormattedDate (production) {
+      return moment(production.startTime).format('MMM Do YYYY')
+    }
+  },
   apollo: {
     productions: gql`query { productions {
-        name
         id
+        description
+        name
+        thumbnail {
+            name
+            link
+        }
+        startTime
 }}`
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  /* Fix word wrapping in cards - See vuetifyjs/vuetify/issues/9130 */
+  .v-card__text, .v-card__title {
+    word-break: normal;
+  }
+
   .past-prod-list {
     white-space: nowrap;
     overflow-x: scroll;
@@ -33,6 +73,10 @@ export default {
       display: inline-block;
       width: 300px;
       margin-right: 20px;
+    }
+
+    .prod-actions {
+      float: right;
     }
   }
 </style>
