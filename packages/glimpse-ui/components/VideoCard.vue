e7e7ae3197cb5062e9d94c0d42010ec13e3241f3
bugfroggy <erik@bugg.co>
2020-04-11 02:31:23 -0400

    Finished RecentProductionsList.vue data fetching Added VideoCard.vue Added sample data to NextLivestream.vue and VideoCard.vue for demonstration purposes and proof-of-concept Fixed .gitignore typo Added proxy for static content served by server

---
+++ packages/glimpse-ui/components/VideoCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="aspect-ratio-maintain">
+    <!-- TODO - Added for demonstration -->
+    <iframe
+      src="https://www.youtube.com/embed/shqIYTVrFvo"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+export default {
+  name: 'VideoCard',
+  props: {
+    videoId: {
+      type: Number,
+      default: 0
+    }
+  },
+  apollo: {
+    video: {
+      query: gql`
+        query getVideo($id: Int!) {
+          video:getVideo(id: $id) {
+            data
+            id
+            name
+            videoType
+          }
+        }`,
+      variables () {
+        return {
+          id: this.videoId
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .aspect-ratio-maintain {
+    position: relative;
+    width: 100%;
+
+    iframe {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      border: none;
+    }
+  }
+</style>
