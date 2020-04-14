8640675eefd22b78b11a5adc21ff91a0e9e64d53
Erik <robere2@rpi.edu>
2020-04-13 21:02:52 -0400

    Further worked on authentication. Server is sending responses and client is reading them, only thing left is session & cookies
    Fixed an issue with getVideo still having the "data" field even though that was removed.
    
    Added /api proxy
    
    See issue rpitv/glimpse-api#3

--- pages/login.vue
+++ pages/login.vue
@@ -1,29 +1,53 @@
 <template>
   <div class="container">
     <VCard class="logging-in-card">
-      <VCardTitle v-if="!this.$route.query.ticket" class="title">
-        Redirecting...
-      </VCardTitle>
-      <VCardTitle v-else>
-        Logging you in...
-      </VCardTitle>
+      <span v-if="error === ''">
+        <VCardTitle>
+          Logging you in...
+        </VCardTitle>
 
-      <VCardSubtitle>Please wait!</VCardSubtitle>
-      <VCardText>
-        <VProgressCircular indeterminate color="primary" />
-      </VCardText>
+        <VCardSubtitle>Please wait!</VCardSubtitle>
+        <VCardText>
+          <VProgressCircular indeterminate color="primary" />
+        </VCardText>
+      </span>
+      <span v-else>
+        <VCardTitle>
+          Womp womp!
+        </VCardTitle>
+
+        <VCardSubtitle>Something went wrong! Here's an error code: {{ error }}</VCardSubtitle>
+        <VCardSubtitle>Try again, perhaps.</VCardSubtitle>
+      </span>
     </VCard>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   middleware: 'login',
   data () {
     return {
       title: 'Login - RPI TV',
-      description: 'Log into the RPI TV website via RPI CAS.'
+      description: 'Log into the RPI TV website via RPI CAS.',
+      error: ''
+    }
+  },
+  async mounted () {
+    try {
+      await axios.post('/api/login', {
+        ticket: this.$route.query.ticket
+      }, { withCredentials: true })
+
+      await this.$router.push('/')
+    } catch (e) {
+      if (e.response && e.response.data && e.response.data.error) {
+        this.error = e.response.data.error
+      } else {
+        this.error = e.response.statusText.toUpperCase()
+      }
     }
   },
   head () {
