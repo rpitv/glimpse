37ff2f8b930442ecba81151a5b5d1fa8705db790
Erik <robere2@rpi.edu>
2020-05-05 16:46:36 -0400

    Added new header test Fixed bug with header button custom classes

--- test/components/TheHeader.spec.js
+++ test/components/TheHeader.spec.js
@@ -12,6 +12,147 @@ Vue.use(Vuex)
 Vue.use(Vuetify)
 const vuetifyInstance = new Vuetify(VuetifyOptions)
 
+describe('TheHeader (Custom)', () => {
+  let wrapper
+  const data = {
+    menuItems: {
+      'LEFT': [
+        {
+          'title': 'Sample Left #1',
+          'path': '/samplel1',
+          'icon': ['fal', 'sample-l1'],
+          'sublist': [],
+          'customClass': 'sample-class-left1',
+          'conditional': () => true
+        },
+        {
+          'title': 'Sample Left #2',
+          'path': '/samplel2',
+          'icon': ['fal', 'sample-l2'],
+          'sublist': [],
+          'customClass': 'sample-class-left2',
+          'conditional': null
+        }
+      ],
+      'RIGHT': [
+        {
+          'title': 'SampleRight #1',
+          'path': '/sampler1',
+          'icon': ['fal', 'sample-r1'],
+          'sublist': [],
+          'customClass': 'sample-r1',
+          'conditional': () => true
+        },
+        {
+          'title': 'SampleRight #2',
+          'path': '/sampler2',
+          'icon': ['fal', 'sample-r2'],
+          'sublist': [],
+          'customClass': 'sample-r2',
+          'conditional': () => true
+        },
+        {
+          'title': 'SampleRight #3',
+          'path': '/sampler3',
+          'icon': ['fal', 'sample-r3'],
+          'sublist': [],
+          'customClass': 'sample-r3',
+          'conditional': () => false
+        }
+      ]
+    }
+  }
+
+  function mount (opts) {
+    wrapper = shallowMount(TheHeader, {
+      ...opts,
+      vuetify: vuetifyInstance,
+      router: new VueRouter(),
+      stubs: {
+        NuxtLink: RouterLinkStub
+      }
+    })
+    wrapper.setData(data)
+  }
+  let store
+  beforeEach(() => {
+    store = new Vuex.Store({
+      getters: {
+        isAuthenticated: () => false
+      }
+    })
+  })
+
+  test('Properly renders', async () => {
+    mount({ store })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.element).toMatchSnapshot()
+    expect(wrapper.findAll('headerbutton-stub').length).toEqual(4)
+  })
+
+  test('Items contain correct attributes', async (done) => {
+    mount({ store })
+    await wrapper.vm.$nextTick()
+    const leftButtons = wrapper.findAll('vappbar-stub > vtoolbaritems-stub .item-wrapper')
+    const rightButtons = wrapper.findAll('.app-nav-right-items .item-wrapper')
+    for (let i = 0; i < leftButtons.length; i++) {
+      const buttonWrapper = leftButtons.at(i)
+      const button = buttonWrapper.find('headerbutton-stub')
+      let found = false
+
+      if (!button.exists()) {
+        continue
+      }
+
+      for (let j = 0; j < data.menuItems.LEFT.length; j++) {
+        if (data.menuItems.LEFT[j].customClass &&
+          buttonWrapper.classes().includes(data.menuItems.LEFT[j].customClass) &&
+          button.attributes('path') === data.menuItems.LEFT[j].path &&
+          button.attributes('title') === data.menuItems.LEFT[j].title &&
+          (!data.menuItems.LEFT[j].conditional || data.menuItems.LEFT[j].conditional())
+        ) {
+          found = true
+          break
+        }
+      }
+
+      if (!found) {
+        done.fail('Left button exists even though no valid match was found in the menuItems. Button: ' +
+          buttonWrapper.element.outerHTML)
+      }
+    }
+
+    for (let i = 0; i < rightButtons.length; i++) {
+      const buttonWrapper = rightButtons.at(i)
+      const button = buttonWrapper.find('headerbutton-stub')
+      let found = false
+
+      if (!button.exists()) {
+        continue
+      }
+
+      for (let j = 0; j < data.menuItems.RIGHT.length; j++) {
+        if (data.menuItems.RIGHT[j].customClass &&
+            buttonWrapper.classes().includes(data.menuItems.RIGHT[j].customClass) &&
+            button.attributes('path') === data.menuItems.RIGHT[j].path &&
+            button.attributes('title') === data.menuItems.RIGHT[j].title &&
+            (!data.menuItems.RIGHT[j].conditional || data.menuItems.RIGHT[j].conditional())
+        ) {
+          found = true
+          break
+        }
+      }
+
+      if (!found) {
+        done.fail('Right button exists even though no valid match was found in the menuItems. Button: ' +
+          buttonWrapper.element.outerHTML)
+      }
+    }
+
+    done()
+  })
+})
+
 describe('TheHeader (Unauthenticated)', () => {
   let wrapper
 
