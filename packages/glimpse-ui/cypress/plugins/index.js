2e1ba22f885ee603da0d62d16d7addd7083502c8
Erik <robere2@rpi.edu>
2020-05-07 19:15:12 -0400

    Work on adding first test case and adding GraphQL mocking

--- packages/glimpse-ui/cypress/plugins/index.js
+++ packages/glimpse-ui/cypress/plugins/index.js
@@ -12,10 +12,22 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const path = require('path')
+const fs = require('fs-extra')
+
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+module.exports = (on) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  on('task', {
+    getSchema () {
+      return fs.readFileSync(
+        path.resolve(__dirname, '../../api-schema.graphql'),
+        'utf8'
+      )
+    }
+  })
 }
--- packages/glimpse-ui/cypress/support/commands.js
+++ packages/glimpse-ui/cypress/support/commands.js
@@ -1,3 +1,5 @@
+import 'cypress-graphql-mock'
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
