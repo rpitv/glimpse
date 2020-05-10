07408ab8e57862e044e3d5ab5ee9a257a1a5cfef
Erik <robere2@rpi.edu>
2020-05-09 20:46:08 -0400

    Remove graphql-cypress-mock in favor of custom solution

--- packages/glimpse-ui/cypress/support/commands.js
+++ packages/glimpse-ui/cypress/support/commands.js
@@ -1,5 +1,3 @@
-import 'cypress-graphql-mock'
-
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -25,3 +23,37 @@ import 'cypress-graphql-mock'
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('mockGraphql', (operationStubs) => {
+  cy.on('window:before:load', (win) => {
+    cy.stub(win, 'fetch', (...args) => {
+      console.log('Handling fetch stub', args)
+      const [url, request] = args
+      const postBody = JSON.parse(request.body)
+      let promise
+
+      if (url.includes('/api/graphql')) {
+        operationStubs.some((stub) => {
+          if (postBody.operationName === stub.operation) {
+            console.log('STUBBING', stub.operation)
+            promise = Promise.resolve({
+              ok: true,
+              text () {
+                return Promise.resolve(JSON.stringify(stub.response))
+              }
+            })
+            return true
+          }
+          return false
+        })
+      }
+
+      if (promise) {
+        return promise
+      }
+
+      console.log('Could not find a stub for the operation.')
+      return false
+    })
+  })
+})
