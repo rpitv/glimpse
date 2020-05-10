2827e3a01d9ab8140531c5ee7898535ddf85e394
Erik <robere2@rpi.edu>
2020-05-09 20:50:20 -0400

    Remove examples add ignored files for eslint

--- packages/glimpse-ui/cypress/integration/sample_spec.js
+++
@@ -1,8 +0,0 @@
-describe('My first test', () => {
-  it('Clickss an element', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('type').click()
-
-    cy.url().should('include', '/commands/actions')
-  })
-})
