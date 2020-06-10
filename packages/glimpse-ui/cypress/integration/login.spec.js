1c2854b8a85536d091673e41d0757f74eb7847b6
Erik <robere2@rpi.edu>
2020-06-09 23:59:32 -0400

    Fixed ESLint errors

--- packages/glimpse-ui/cypress/integration/login.spec.js
+++ packages/glimpse-ui/cypress/integration/login.spec.js
@@ -1,5 +1,4 @@
 describe('Login Process', () => {
-
   it('Redirects to RPI CAS when unauthed w/ no ticket', function () {
     cy.visit('/login')
     cy.location('origin').should('eq', 'https://cas-auth.rpi.edu')
@@ -7,9 +6,9 @@ describe('Login Process', () => {
 
   it('Authenticates when unauthed w/ valid ticket (Admin)', function () {
     cy.server()
-    cy.route('POST', '/api/auth/login', {rcs_id: 'user1', admin: true})
+    cy.route('POST', '/api/auth/login', { rcs_id: 'user1', admin: true })
     cy.visit('/login?ticket=valid-ticket-1')
-    cy.location('pathname', {timeout: 10000}).should('eq', '/')
+    cy.location('pathname', { timeout: 10000 }).should('eq', '/')
     cy.get('.app-nav-right-items').should(($el) => {
       expect($el.text()).to.contain('user1@rpi.edu')
     })
@@ -21,9 +20,9 @@ describe('Login Process', () => {
 
   it('Authenticates when unauthed w/ valid ticket (Non-Admin)', function () {
     cy.server()
-    cy.route('POST', '/api/auth/login', {rcs_id: 'user2', admin: true})
+    cy.route('POST', '/api/auth/login', { rcs_id: 'user2', admin: true })
     cy.visit('/login?ticket=valid-ticket-2')
-    cy.location('pathname', {timeout: 10000}).should('eq', '/')
+    cy.location('pathname', { timeout: 10000 }).should('eq', '/')
     cy.get('.app-nav-right-items').should(($el) => {
       expect($el.text()).to.contain('user2@rpi.edu')
     })
@@ -37,7 +36,7 @@ describe('Login Process', () => {
     cy.server()
     cy.route({
       method: 'POST',
-      response: {error: 'SAMPLE_ERROR'},
+      response: { error: 'SAMPLE_ERROR' },
       url: '/api/auth/login',
       status: 400
     })
