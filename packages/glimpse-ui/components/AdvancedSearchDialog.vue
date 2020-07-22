c18ff9ffc6f36667c21368cd2194e899e216a451
Erik <robere2@rpi.edu>
2020-07-21 20:48:00 -0400

    Add examples to advanced search rules

--- packages/glimpse-ui/components/AdvancedSearchDialog.vue
+++ packages/glimpse-ui/components/AdvancedSearchDialog.vue
@@ -10,23 +10,72 @@
 
       <VCardText>
         <p>Advanced searching is supported in all search menus on the RPI TV website by using a special syntax.</p>
-        <p>Hover above the rules below for examples of how to use them.</p>
+        <p>
+          An easy-to-use UI will be introduced in the future, replacing the need to manually type in advanced searches.
+          In the mean time, hover above the rules below for examples of how to use them.
+        </p>
         <ul class="syntax-list">
-          <li>Searches are split into words at each space (<kbd>&nbsp;</kbd>). Searches match if any field contains ONE OF the words.</li>
           <li>
-            Words are "optional", unless followed by a <kbd>!</kbd>.
+            <VTooltip top>
+              <template v-slot:activator="{ on }">
+                <p v-on="on">
+                  Searches are split into words at each space (<kbd>&nbsp;</kbd>).
+                  Searches match if any field contains ONE OF the words.
+                </p>
+              </template>
+              <span><code>One Two</code> will require results to either contain "One" OR "Two".</span>
+            </VTooltip>
           </li>
           <li>
-            You can search for specific fields by prepending the field name, followed by a <kbd>:</kbd>.
+            <VTooltip top>
+              <template v-slot:activator="{ on }">
+                <p v-on="on">
+                  Words are "optional", unless followed by a <kbd>!</kbd>.
+                </p>
+              </template>
+              <span><code>One! Two!</code> will require results to contain both "One" AND "Two".</span>
+            </VTooltip>
           </li>
           <li>
-            You can search for a number range by putting a <kbd>-</kbd> between two numbers.
+            <VTooltip top>
+              <template v-slot:activator="{ on }">
+                <p v-on="on">
+                  You can search for specific fields by prepending the field name, followed by a <kbd>:</kbd>.
+                </p>
+              </template>
+              <span><code>One name:Two</code> will return results which have a name which matches "Two",
+                OR which match "One" in any field.</span>
+            </VTooltip>
           </li>
           <li>
-            You can include spaces in your search by wrapping your search in two <kbd>"</kbd> characters.
+            <VTooltip top>
+              <template v-slot:activator="{ on }">
+                <p v-on="on">
+                  You can search for a number range by putting a <kbd>-</kbd> between two numbers.
+                </p>
+              </template>
+              <span><code>20-22</code> will return results which match 20, 21, OR 22.</span>
+            </VTooltip>
           </li>
           <li>
-            You can escape any of the above special characters by prepending them with a <kbd>/</kbd>.
+            <VTooltip top>
+              <template v-slot:activator="{ on }">
+                <p v-on="on">
+                  You can include spaces in your search words by wrapping your search in two <kbd>"</kbd> characters.
+                </p>
+              </template>
+              <span><code>"One Two" Three</code> will return results which match "One Two" OR "Three"</span>
+            </VTooltip>
+          </li>
+          <li>
+            <VTooltip top>
+              <template v-slot:activator="{ on }">
+                <p v-on="on">
+                  You can escape any of the above special characters by prepending them with a <kbd>/</kbd>.
+                </p>
+              </template>
+              <span><code>One\!</code> will return results which match "One!"</span>
+            </VTooltip>
           </li>
         </ul>
         <div v-if="fields" class="this-fields">
@@ -87,7 +136,9 @@ export default {
   .this-fields {
     margin-top: 20px;
   }
-  .syntax-list {
-    line-height: 2em;
+
+  .syntax-list p {
+    margin: 5px;
+    font-weight: bold;
   }
 </style>
