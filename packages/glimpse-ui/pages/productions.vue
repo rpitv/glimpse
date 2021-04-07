16269463789ecb3395854a1d6bec2279b4997c08
Erik <robere2@rpi.edu>
2021-04-07 18:53:22 -0400

    Added infinite scrolling

--- packages/glimpse-ui/pages/productions.vue
+++ packages/glimpse-ui/pages/productions.vue
@@ -1,22 +1,46 @@
 <template>
-  <div class="container">
-    <h1 class="title">
-      Productions
-    </h1>
-    <div class="cards">
-      <div v-for="prod in productions" class="rowitem">
-        <ProductionCard
-          :production-id="parseInt(prod.id)"
-          :name="prod.name"
-          :thumbnail="prod.thumbnail.link"
-          :start="prod.startTime"
+  <div class="">
+    <VRow>
+      <VCol cols="12" md="4" lg="3" offset-md="1" class="filters">
+        <VRow>
+          <VCol cols="12" sm="10" offset-sm="1">
+            <h1 class="title">
+              Productions
+            </h1>
+            <h2>Filters</h2>
+            <VTextField @change="searchInput" v-model="searchVal" class="search" label="Search..." />
+          </VCol>
+        </VRow>
+      </VCol>
+      <VCol cols="12" md="6" lg="7" offset-md="5" offset-lg="4">
+        <div class="cards">
+          <div v-for="prod in productions" class="rowitem">
+            <ProductionCard
+              :production-id="parseInt(prod.id)"
+              :name="prod.name"
+              :thumbnail="prod.thumbnail.link"
+              :start="prod.startTime"
+            />
+          </div>
+        </div>
+      </VCol>
+    </VRow>
+    <VRow>
+      <VCol
+        class="page-bottom"
+      >
+        <div
+          v-intersect="onIntersect"
+          class="end"
         />
-      </div>
-      <div
-        v-intersect="onIntersect"
-        class="end"
-      />
-    </div>
+        <div v-if="loadingMore">
+          <VProgressCircular :size="70" :width="7" indeterminate color="primary" />
+          <p class="loading-text">
+            Loading...
+          </p>
+        </div>
+      </VCol>
+    </VRow>
   </div>
 </template>
 
@@ -30,7 +54,12 @@ export default {
     return {
       title: 'Our Productions - RPI TV',
       description: 'List of all of RPI TV\'s past productions',
-      productions: []
+      searchVal: '',
+      productions: [],
+      loadingMore: false,
+      prevPage: 0,
+      // Note: the numbers 300 and 168.75 are important; they are the width & height of each individual card.
+      countPerPage: Math.floor((this.$vuetify.breakpoint.width / 300) * (this.$vuetify.breakpoint.height / 168.75))
     }
   },
   head () {
@@ -45,14 +74,11 @@ export default {
     }
   },
   methods: {
-    onIntersect () {
-      console.log('Intersect!')
-    }
-  },
-  apollo: {
-    productions: {
-      query: gql`query ProductionsGetProductions {
-        productions: productions(pageSize: 500) {
+    async onIntersect () {
+      this.loadingMore = true
+      const result = await this.$apollo.query({
+        query: gql`query ProductionsGetProductions($prevProductionIndex: Int!, $pageSize: Int!) {
+        productions: productions(pageSize: $pageSize, prevProductionIndex: $prevProductionIndex) {
           id
           name
           startTime
@@ -60,19 +86,58 @@ export default {
             link
           }
         }
-      }`
+      }`,
+        variables: {
+          prevProductionIndex: this.prevPage * this.countPerPage,
+          pageSize: this.countPerPage
+        }
+      })
+      this.prevPage++
+      this.productions = this.productions.concat(result.data.productions)
+      this.loadingMore = false
+    },
+    searchInput (v) {
+      console.log(v)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .cards {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(350px, max-content));
+
+@import '~vuetify/src/styles/styles';
+.cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(320px, max-content));
+  justify-content: left;
+  @media(max-width: map-get($grid-breakpoints, 'md')) {
     justify-content: center;
   }
-  .rowitem {
-    margin-bottom: 20px;
+}
+.title {
+  margin-bottom: 30px;
+}
+.rowitem {
+  @media(max-width: map-get($grid-breakpoints, 'md')) {
+    margin-left: 10px;
   }
+  margin-bottom: 20px;
+}
+.end {
+  position: absolute;
+  bottom: 0;
+  padding-top: 30px;
+}
+.filters {
+  position: fixed;
+  @media(max-width: map-get($grid-breakpoints, 'md')) {
+    position: inherit;
+  }
+}
+.page-bottom {
+  text-align: center;
+}
+.loading-text {
+  margin-top: 20px;
+}
 </style>
