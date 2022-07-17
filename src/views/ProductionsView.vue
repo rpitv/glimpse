1b06cb68e16643e298ecfee4623af6922755ec4d
Erik Roberts <me@ecr.dev>
2022-07-16 21:20:04 -0400

    Add ProductionCard.vue component

--- src/views/ProductionsView.vue
+++ src/views/ProductionsView.vue
@@ -6,8 +6,10 @@
       </template>
     </n-spin>
   </div>
-  <div v-else>
-    <pre>{{ response.result }}</pre>
+  <div v-else class="production-cards">
+    <ProductionCard v-for="production in response.result.value?.productions" class="production-card" :key="production.id"
+    :id="parseInt(production.id)" :name="production.name" :description="production.description" :start-time="new Date(production.startTime)"
+    :thumbnail-url="production.thumbnail?.path ?? undefined"/>
   </div>
 </template>
 
@@ -15,6 +17,7 @@
 import {NSpin} from "naive-ui";
 import {useQuery} from "@vue/apollo-composable";
 import {FindAllProductionsDocument} from "@/graphql/types";
+import ProductionCard from "@/components/ProductionCard.vue";
 
 let pageSize = 20;
 
@@ -30,5 +33,13 @@ const response = useQuery(FindAllProductionsDocument, {
   display: flex;
   justify-content: center;
   padding-top: 5em;
+}
+
+.production-cards {
+  padding: 7vw;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 300px);
+  grid-gap: 1em;
+  justify-content: center;
 }
 </style>
