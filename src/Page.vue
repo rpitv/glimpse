811f4f5d85eb150a4967929d5203ab83349bc62f
Erik Roberts <me@ecr.dev>
2022-07-26 22:10:51 -0400

    replace NavigationHeader NMenu components with custom navbar (WIP)

--- src/Page.vue
+++ src/Page.vue
@@ -1,15 +1,15 @@
 <template>
   <!-- Background -->
-  <div class="background" />
-  <BackgroundShape :class="layoutCssName + ' layout background-shape'" />
+  <div class="background"/>
+  <BackgroundShape :class="layoutCssName + ' layout background-shape'"/>
 
   <!-- Navbar -->
-  <NavigationHeader />
+  <NavigationHeader/>
 
   <!-- Logo -->
   <div class="main-logo-wrapper">
     <img :class="layoutCssName + ' layout main-logo'" :data-scroll="scrollY"
-         alt="RPI TV logo" src="@/assets/rpitv_logo.svg" />
+         alt="RPI TV logo" src="@/assets/rpitv_logo.svg"/>
   </div>
 
   <!-- Page content -->
@@ -18,9 +18,9 @@
       <Transition name="router">
         <div :class="layoutCssName + ' layout router-page'" :key="route.name">
           <div>
-            <component :is="Component" />
+            <component :is="Component"/>
           </div>
-          <Footer />
+          <Footer/>
         </div>
       </Transition>
     </RouterView>
@@ -32,9 +32,9 @@
 import NavigationHeader from "@/components/NavigationHeader.vue";
 import BackgroundShape from "@/components/BackgroundShape.vue";
 import Footer from "@/components/Footer.vue";
-import { onMounted, onUnmounted, ref, watch } from "vue";
-import { useLoadingBar, useMessage } from "naive-ui";
-import { useRoute } from "vue-router";
+import {onMounted, onUnmounted, ref, watch} from "vue";
+import {useLoadingBar, useMessage} from "naive-ui";
+import {useRoute} from "vue-router";
 import {useAuthStore} from "@/stores/auth";
 
 const route = useRoute();
@@ -43,16 +43,18 @@ const authStore = useAuthStore();
 
 // Fetch identity and permissions from server
 const ownId = await authStore.getOwnId();
-if(typeof ownId === "number") {
+if (typeof ownId === "number") {
   authStore.isLoggedIn = true;
 }
 await authStore.getPermissions();
 
 // Listen for scrolling to update the logo size
 const scrollY = ref(window.scrollY);
+
 function updateScroll() {
   scrollY.value = window.scrollY;
 }
+
 onMounted(() => window.addEventListener("scroll", updateScroll));
 onUnmounted(() => window.removeEventListener("scroll", updateScroll));
 
@@ -60,20 +62,20 @@ const loadingBar = useLoadingBar();
 // Listen to all fetch calls for the loading bar
 // https://stackoverflow.com/questions/38995750/how-to-listen-on-all-fetch-api-calls
 let pendingFetches = 0;
-(function(ns, fetch) {
+(function (ns, fetch) {
   if (typeof fetch !== "function") {
     return;
   }
-  ns.fetch = function() {
+  ns.fetch = function () {
     // @ts-ignore
     const out = fetch.apply(this, arguments);
 
-    if(pendingFetches++ === 0) {
+    if (pendingFetches++ === 0) {
       loadingBar.start();
     }
     out
       .then(() => {
-        if(--pendingFetches === 0) {
+        if (--pendingFetches === 0) {
           loadingBar.finish()
         }
       })
@@ -95,7 +97,7 @@ watch(() => route.meta.layoutCssName,
     try {
       layoutCssName.value = <string>route.meta.layoutCssName;
     } catch (e) {
-      console.error("Error!");
+      console.error(e);
     }
   });
 </script>
--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -1,23 +1,47 @@
 <template>
-  <header class="nav-wrapper">
-    <nav class="glimpse-main-nav">
-      <n-menu class="left-nav" mode="horizontal" :options="leftNavOptions" />
-      <n-menu class="right-nav" mode="horizontal" :options="rightNavOptions" />
+  <header>
+    <nav class="navbar" aria-label="Website Navigation">
+      <ul class="navbar-section left">
+        <li v-for="button in leftButtons">
+          <RouterLink :to="{name: button.route}" :aria-current="route.name === button.route ? 'page' : undefined">
+            <n-button class="nav-button" :type="route.name === button.route ? 'primary' : 'default'" large quaternary>
+              <template #icon v-if="button.showIconOnDesktop || windowWidth < 500">
+                <FontAwesomeIcon :icon="['fal', button.icon]" />
+              </template>
+              {{button.name}}
+            </n-button>
+          </RouterLink>
+        </li>
+      </ul>
+
+      <ul class="navbar-section right">
+        <li v-for="button in rightButtons">
+          <RouterLink :to="{name: button.route}" :aria-current="route.name === button.route ? 'page' : undefined">
+            <n-button class="nav-button" :type="route.name === button.route ? 'primary' : 'default'" large quaternary>
+              <template #icon v-if="button.showIconOnDesktop || windowWidth < 500">
+                <FontAwesomeIcon :icon="['fal', button.icon]" />
+              </template>
+              {{button.name}}
+            </n-button>
+          </RouterLink>
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
 
 <script setup lang="ts">
-import type { MenuOption } from "naive-ui";
-import { NMenu } from "naive-ui";
-import { h, ref, onMounted, onUnmounted, computed } from "vue";
-import { RouterLink } from "vue-router";
+import { NButton } from "naive-ui";
+import {h, ref, onMounted, onUnmounted, computed, Ref} from "vue";
+import {RouterLink, useRoute} from "vue-router";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import LoginLogoutPopupButton from "./LoginLogoutPopupButton.vue";
 import {useAuthStore} from "@/stores/auth";
+import {requirePermission} from "@/casl";
 
 // Import composables
 const authStore = useAuthStore();
+const route = useRoute();
 
 // Used in v-bind in <style>
 const navbarColor = computed(() => {
@@ -38,165 +62,77 @@ onUnmounted(() => document.removeEventListener("scroll", updateScroll));
 onMounted(() => window.addEventListener("resize", updateWindowWidth));
 onUnmounted(() => window.removeEventListener("resize", updateWindowWidth));
 
-// Compute items on left side of navigation bar
-const leftNavOptions = computed(() => {
-  // Width < 500 -- Phones
-  // Navigation is along the bottom
-  if (windowWidth.value < 500) {
-    return [
-      createButton("Home", "home", "home"),
-      createButton("Productions", "productions", "film"),
-      createButton("More", undefined, "ellipsis", [
-        createButton("Login", "login", "arrow-right-to-arc"),
-        createButton("Contact Us", "contact", "envelope"),
-        createButton("About", "about", "circle-info"),
-        createButton("Join the Club", "join", "people-group"),
-        createButton("Donate", "donate", "book-heart")
-      ])
-    ];
-  }
-  // 500 < width < 550 -- Large phones / small tablets
-  // Nav is at the top, logo on left and login on right, only enough space for 2 buttons
-  if (500 <= windowWidth.value && windowWidth.value < 550) {
-    return [
-      createButton("Home", "home", "home"),
-      createButton("More", undefined, "ellipsis", [
-        createButton("Productions", "productions", "film"),
-        createButton("Contact Us", "contact", "envelope"),
-        createButton("About", "about", "circle-info"),
-        createButton("Join the Club", "join", "people-group"),
-        createButton("Donate", "donate", "book-heart")
-      ])
-    ];
-  }
-  // 550 < width < 675 -- Tablets
-  // Nav is at the top, logo on left and login on right, enough space for 3 buttons
-  if (550 <= windowWidth.value && windowWidth.value < 675) {
-    return [
-      createButton("Home", "home", "home"),
-      createButton("Productions", "productions"),
-      createButton("More", undefined, "ellipsis", [
-        createButton("Contact Us", "contact", "envelope"),
-        createButton("About", "about", "circle-info"),
-        createButton("Join the Club", "join", "people-group"),
-        createButton("Donate", "donate", "book-heart")
-      ])
-    ];
-  }
-  // 675 < width < 800 -- Tablets
-  // Nav is at the top, logo on left and login on right, enough space for 4 buttons
-  if (675 <= windowWidth.value && windowWidth.value < 800) {
-    return [
-      createButton("Home", "home", "home"),
-      createButton("Productions", "productions"),
-      createButton("Contact Us", "contact", "envelope"),
-      createButton("More", undefined, "ellipsis", [
-        createButton("About", "about", "circle-info"),
-        createButton("Join the Club", "join", "people-group"),
-        createButton("Donate", "donate", "book-heart")
-      ])
-    ];
-  }
-  // 800 < width < 1000 -- Small computers
-  // Nav is at the top, logo centered and login on right, enough space for only 3 buttons
-  if (800 <= windowWidth.value && windowWidth.value < 1000) {
-    return [
-      createButton("Home", "home", "home"),
-      createButton("Productions", "productions"),
-      createButton("More", undefined, "ellipsis", [
-        createButton("Contact Us", "contact", "envelope"),
-        createButton("About", "about", "circle-info"),
-        createButton("Join the Club", "join", "people-group"),
-        createButton("Donate", "donate", "book-heart")
-      ])
-    ];
-  }
-  // 1000 < width < 1200 -- Computers
-  // Nav is at the top, logo centered and login on right, enough space for 4 buttons
-  if (1000 <= windowWidth.value && windowWidth.value < 1200) {
-    return [
-      createButton("Home", "home", "home"),
-      createButton("Productions", "productions"),
-      createButton("Contact Us", "contact"),
-      createButton("More", undefined, "ellipsis", [
-        createButton("About", "about", "circle-info"),
-        createButton("Join the Club", "join", "people-group"),
-        createButton("Donate", "donate", "book-heart")
-      ])
-    ];
-  }
-  // 1200 < width < 1400 -- Computers
-  // Nav is at the top, logo centered and login on right, enough space for 5 buttons
-  if (1200 <= windowWidth.value && windowWidth.value < 1400) {
-    return [
-      createButton("Home", "home", "home"),
-      createButton("Productions", "productions"),
-      createButton("Contact Us", "contact"),
-      createButton("About", "about"),
-      createButton("More", undefined, "ellipsis", [
-        createButton("Join the Club", "join", "people-group"),
-        createButton("Donate", "donate", "book-heart")
-      ])
-    ];
-  }
 
-  // 1600 < width -- Large computers
-  // Nav is at the top, logo centered and login on right, enough space for 6 buttons
-  return [
-    createButton("Home", "home", "home"),
-    createButton("Productions", "productions"),
-    createButton("Contact Us", "contact"),
-    createButton("About", "about"),
-    createButton("Join the Club", "join"),
-    createButton("Donate", "donate")
-  ];
-});
-
-// Compute items on right side of navigation bar
-const rightNavOptions = computed(() => {
-  // Tablets and larger. Navbar is used
-  if (500 <= windowWidth.value) {
-    return [
-      createButton("Login", "login", "arrow-right-to-arc")
-    ];
-  }
-  // Phones. Everything in "more" dropdown
-  return [];
-});
-
-/**
- * Create a button for the navbar
- * @param name Text to display on this button.
- * @param to Name of router page that this button should open, or undefined if none. If "login", this is a special
- *   case that will open the login modal when clicked.
- * @param icon Name of the FontAwesome icon, or undefined if none.
- * @param children Children MenuOptions to show in the dropdown.
- */
-function createButton(name: string, to?: string, icon?: string, children?: MenuOption[]): MenuOption {
-  if(to === "login") {
-    return {
-      label: () => h(
-        LoginLogoutPopupButton,
-        {},
-        {default: () => authStore.isLoggedIn ? "Logout" : "Login"}
-      ),
-      icon: icon ? () => h(FontAwesomeIcon, {icon: ["fal", authStore.isLoggedIn ? "arrow-right-from-arc" : "arrow-right-to-arc"]}) : undefined,
-      key: name,
-      children: children
-    };
-  } else {
-    return {
-      label: () => h(
-        RouterLink,
-        {to: {name: to}},
-        {default: () => name}
-      ),
-      icon: icon ? () => h(FontAwesomeIcon, {icon: ["fal", icon]}) : undefined,
-      key: name,
-      children: children
-    };
-  }
+type NavButton = {
+  name: string;
+  icon: string;
+  showIconOnDesktop: boolean;
+  route?: string;
+  children?: NavButton[];
+  side: string;
+  visible?: (() => Ref<boolean>);
 }
+
+const buttons: Ref<NavButton[]> = ref([
+  {
+    name: "Home",
+    icon: "home",
+    showIconOnDesktop: true,
+    route: "home",
+    side: "left"
+  },
+  {
+    name: "Productions",
+    icon: "film",
+    showIconOnDesktop: false,
+    route: "productions",
+    side: "left",
+    visible: requirePermission("read", "Production")
+  },
+  {
+    name: "About",
+    icon: "info-circle",
+    showIconOnDesktop: false,
+    route: "about",
+    side: "left"
+  },
+  {
+    name: "Contact Us",
+    icon: "envelope",
+    showIconOnDesktop: false,
+    route: "contact",
+    side: "left",
+    visible: requirePermission("create", "ContactSubmission")
+  },
+  {
+    name: "Join the Club",
+    icon: "people-group",
+    showIconOnDesktop: false,
+    route: "join",
+    side: "left"
+  },
+  {
+    name: "Donate",
+    icon: "book-heart",
+    showIconOnDesktop: false,
+    route: "donate",
+    side: "left"
+  },
+  { /* Login is a special case, icon and route are overwritten. */
+    name: "Login",
+    icon: "arrow-right-to-arc",
+    showIconOnDesktop: true,
+    route: "login",
+    side: "right"
+  }
+]);
+
+const leftButtons = computed(() => {
+  return buttons.value.filter(button => button.side === "left");
+});
+const rightButtons = computed(() => {
+  return buttons.value.filter(button => button.side === "right");
+});
 
 function updateScroll(): void {
   scrollPos.value = window.scrollY;
@@ -209,35 +145,36 @@ function updateWindowWidth() {
 
 <style scoped lang="scss">
 header {
-  display: flex;
-  justify-content: center;
   z-index: 1;
 }
 
-img {
-  position: fixed;
-  z-index: 2;
-  pointer-events: none;
-}
-
-nav {
+.navbar {
+  padding: 0 10px;
+  display: flex;
   position: fixed;
   width: 100%;
+}
+
+.navbar-section {
   display: flex;
   align-items: center;
+  list-style: none;
+}
+
+.nav-button {
+  font-size: 1.2em;
 }
 
 @media(min-width: 500px) {
-  .right-nav {
-    position: absolute;
-    right: 0;
+  .navbar {
+    justify-content: space-between;
   }
 }
 
 // Phone displays
 @media (max-width: 499px) {
   // Move navbar to the bottom of the screen
-  nav {
+  .navbar {
     bottom: 0;
     top: initial;
     padding: 1em;
@@ -245,68 +182,34 @@ nav {
     background-color: rgb(27, 29, 35);
     height: 5em;
   }
-  // Move image to the center top of the screen
-  img {
-    position: absolute;
-    top: 0;
-    margin: 0;
-    height: 25vw;
-  }
 }
 
 // Tablets & Computers
 @media (min-width: 500px) {
   // Position navbar at top of screen & enable color changing on scroll
-  nav {
-    text-transform: uppercase;
+  .navbar {
     top: 0;
     height: 4em;
     transition: background-color 0.5s, backdrop-filter 0.5s;
     background-color: v-bind(navbarColor);
     backdrop-filter: v-bind(navbarBlur);
   }
+  // Uppercase buttons not on mobile
+  .nav-button {
+    text-transform: uppercase;
+  }
+
+  .navbar-section {
+    max-width: 40vw;
+    overflow: hidden;
+  }
 }
 
 // Tablet displays
 @media (min-width: 500px) and (max-width: 799px) {
-  // Move logo to top left of nav
-  img {
-    height: 4em;
-    margin-left: 0.5em;
-    left: 0;
-  }
   // Move nav over to make room for logo
-  nav {
+  .navbar {
     padding-left: 6em;
-  }
-}
-
-// Computer displays
-@media (min-width: 800px) {
-  // Move logo to top center of screen
-  img {
-    height: 18vw;
-    max-width: 250px;
-    filter: drop-shadow(0.3em 0.3em 0.3em #00000090);
-  }
-}
-</style>
-
-<style lang="scss">
-// Tablet displays and larger - Make menu button font slightly bigger
-@media (min-width: 500px) {
-  .glimpse-main-nav .n-menu-item-content-header {
-    font-size: 1.2em;
-  }
-}
-
-@media (max-width: 499px) {
-  .glimpse-main-nav .n-menu-item-content {
-    display: flex !important;
-    flex-direction: column;
-  }
-  .glimpse-main-nav .n-menu-item-content__icon {
-    margin-right: 0 !important;
   }
 }
 </style>
