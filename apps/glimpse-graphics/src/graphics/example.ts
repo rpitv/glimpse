3c8afab820acaa34adf964c1b2e904cf1fb8ff1d
Erik Roberts <me@ecr.dev>
2022-08-16 14:51:38 -0400

    Remove unused files/packages

--- apps/glimpse-graphics/src/graphics/example.ts
+++
@@ -1,8 +0,0 @@
-import { createHead } from '@vueuse/head';
-import { createApp } from 'vue';
-import App from './example/main.vue';
-
-const app = createApp(App);
-const head = createHead();
-app.use(head);
-app.mount('#app');
