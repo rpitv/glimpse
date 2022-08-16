a3955d894434610636226ebbf50f87bc55aca3b6
Erik Roberts <me@ecr.dev>
2022-08-16 17:32:29 -0400

    Started working on scoreboard with ClockControl.vue

--- apps/glimpse-graphics/src/dashboard/scoreboard/main.vue
+++ apps/glimpse-graphics/src/dashboard/scoreboard/main.vue
@@ -1,25 +1,10 @@
 <template>
-	<div>Yelloq!</div>
+	<ClockControl />
 </template>
 
 <script setup lang="ts">
-import {useReplicant} from 'nodecg-vue-composable';
-import {ref} from 'vue';
-
-const text = ref('Example');
-
-// Helper composable to make accessing/modifying replicants easier.
-// For more information see https://github.com/Dan-Shields/nodecg-vue-composable
-const exampleReplicant = useReplicant(
-	'exampleReplicant',
-	'nodecg-vue-ts-template',
-);
+import ClockControl from "./ClockControl.vue";
 </script>
 
 <style scoped lang="scss">
-div {
-	background-color: blue;
-	width: 50px;
-	height: 50px;
-}
 </style>
--- apps/glimpse-graphics/src/extension/scoreboard.ts
+++
@@ -1,7 +0,0 @@
-import { get as nodecg } from './util/nodecg';
-import { scoreboardCurrentTimeRep } from './util/replicants';
-
-nodecg().log.info('Extension code working!');
-
-// Access/set a replicant (also see ./util/replicants).
-scoreboardCurrentTimeRep.value = 1200 * 1000; // 1200 seconds (20 minutes)
