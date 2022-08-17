eaa7644fadbc330607f44572b3bfb6d0d9d6cde0
Erik Roberts <me@ecr.dev>
2022-08-17 17:56:33 -0400

    Refactor project to have a "Scoreboard" tab with a bunch of panels for different controls

---
+++ apps/glimpse-graphics/src/dashboard/scoreboard-settings/main.vue
@@ -0,0 +1,12 @@
+<template>
+	<n-config-provider :theme="darkTheme">
+		<div>Testing!</div>
+	</n-config-provider>
+</template>
+
+<script setup lang="ts">
+import {NConfigProvider, darkTheme} from "naive-ui";
+</script>
+
+<style scoped lang="scss">
+</style>
