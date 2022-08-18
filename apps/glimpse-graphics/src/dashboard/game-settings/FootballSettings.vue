62a2651a0110871aebf0bd8c96f5ef443ab20075
Erik Roberts <me@ecr.dev>
2022-08-17 21:17:54 -0400

    Added adjustable play clock length

--- apps/glimpse-graphics/src/dashboard/game-settings/FootballSettings.vue
+++ apps/glimpse-graphics/src/dashboard/game-settings/FootballSettings.vue
@@ -2,6 +2,10 @@
 	<h1>Football Settings</h1>
 	<SyncableToggle name="Play Clock" v-model:enabled="playClockEnabledValue"
 					v-model:synced="playClockSyncedValue"/>
+	<div class="mt-10" v-if="playClockEnabledValue">
+		<label :for="playClockLengthId">Period Length</label>
+		<n-input :id="playClockLengthId" placeholder="0:40.0" v-model="playClockLengthValue"/>
+	</div>
 	<SyncableToggle class="mt-10" name="Downs" v-model:enabled="downsEnabledValue"
 					v-model:synced="downsSyncedValue"/>
 	<SyncableToggle class="mt-10" name="Possession" v-model:enabled="possessionEnabledValue"
@@ -11,6 +15,10 @@
 <script setup lang="ts">
 	import SyncableToggle from "./SyncableToggle.vue";
 	import {ref} from "vue";
+	import {v4} from "uuid";
+	import {NInput} from "naive-ui";
+
+	const playClockLengthId = v4();
 
 	const playClockEnabledValue = ref<boolean>(false);
 	const playClockSyncedValue = ref<boolean>(false);
@@ -18,6 +26,8 @@
 	const downsSyncedValue = ref<boolean>(false);
 	const possessionEnabledValue = ref<boolean>(false);
 	const possessionSyncedValue = ref<boolean>(false);
+
+	const playClockLengthValue = ref<string>('');
 </script>
 
 <style scoped lang="scss">
