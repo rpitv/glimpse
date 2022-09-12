b58696340f8dcef77a4333bd4336df84b3e8e2c8
Erik Roberts <me@ecr.dev>
2022-09-12 19:09:25 -0400

    Add command palette, add hiding scoreboard, add anime.js

--- apps/glimpse-graphics/src/extension/scoreboard-clock.ts
+++ apps/glimpse-graphics/src/extension/scoreboard-clock.ts
@@ -1,45 +1,39 @@
-import { get as nodecg } from './util/nodecg';
 import {replicants} from "./util/replicants";
 
 let scoreboardTimer: NodeJS.Timer | null = null;
 let scoreboardTimerLastModified: number | null = null;
 
-nodecg().listenFor('glimpse-graphics.scoreboard.clock.startClock', (): void => {
-	replicants.scoreboard.clock.isRunning.value = true;
-
-	if (scoreboardTimer) {
-		return; // Clock is already running
+replicants.scoreboard.clock.isRunning.on('change', (newValue: boolean) => {
+	// If isRunning is set to true, but the clock is already running, or vice versa, then do nothing.
+	if((newValue && scoreboardTimer !== null) || (!newValue && scoreboardTimer === null)) {
+		return;
 	}
 
-	scoreboardTimerLastModified = Date.now();
+	if(newValue) {
+		scoreboardTimerLastModified = Date.now();
 
-	scoreboardTimer = setInterval(() => {
-		// Stop the clock once it hits zero.
-		if(replicants.scoreboard.clock.time.value <= 0) {
-			nodecg().sendMessage('glimpse-graphics.scoreboard.clock.stopClock');
-			return;
-		}
-
-		// If clock hasn't hit zero yet, update its value.
-		const now = Date.now();
-		if(scoreboardTimerLastModified !== null) {
-			replicants.scoreboard.clock.time.value -= now - scoreboardTimerLastModified;
-			if(replicants.scoreboard.clock.time.value < 0) {
-				replicants.scoreboard.clock.time.value = 0;
+		scoreboardTimer = setInterval(() => {
+			// Stop the clock once it hits zero.
+			if(replicants.scoreboard.clock.time.value <= 0) {
+				replicants.scoreboard.clock.isRunning.value = false;
+				return;
 			}
-		}
-		scoreboardTimerLastModified = now;
-	}, 100);
-});
 
-nodecg().listenFor('glimpse-graphics.scoreboard.clock.stopClock', (): void => {
-	// Stop the clock if it's running.
-	if(replicants.scoreboard.clock.isRunning.value) {
-		replicants.scoreboard.clock.isRunning.value = false;
+			// If clock hasn't hit zero yet, update its value.
+			const now = Date.now();
+			if(scoreboardTimerLastModified !== null) {
+				replicants.scoreboard.clock.time.value -= now - scoreboardTimerLastModified;
+				if(replicants.scoreboard.clock.time.value < 0) {
+					replicants.scoreboard.clock.time.value = 0;
+				}
+			}
+			scoreboardTimerLastModified = now;
+		}, 100);
+	} else {
+		if(scoreboardTimer !== null) {
+			clearInterval(scoreboardTimer);
+			scoreboardTimer = null;
+		}
+		scoreboardTimerLastModified = null;
 	}
-	if(scoreboardTimer !== null) {
-		clearInterval(scoreboardTimer);
-		scoreboardTimer = null;
-	}
-	scoreboardTimerLastModified = null;
-});
+})
