26b4a3c7f8c79bf14d968eae0d7c14bb245cffa6
Erik Roberts <me@ecr.dev>
2022-09-14 18:10:34 -0400

    Add ability to show raw images as graphics

---
+++ apps/glimpse-graphics/src/dashboard/image-editor/main.vue
@@ -0,0 +1,19 @@
+<template>
+	<n-config-provider :theme="darkTheme">
+		<Suspense>
+			<ImageEditor />
+			<template #fallback>
+				Loading...
+			</template>
+		</Suspense>
+	</n-config-provider>
+</template>
+
+<script setup lang="ts">
+
+import {NConfigProvider, darkTheme} from "naive-ui";
+import ImageEditor from "./ImageEditor.vue";
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ apps/glimpse-graphics/src/dashboard/image-selection/ImageSelector.vue
@@ -0,0 +1,41 @@
+<template>
+<n-table>
+	<thead>
+		<tr>
+			<th>Image Name</th>
+			<th>Preview</th>
+			<th>Controls</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr v-for="img in images" :key="img.sum">
+			<td>
+				{{img.base}}
+			</td>
+			<td>
+				<img :src="img.url" alt="Image Preview" />
+			</td>
+			<td>
+				<n-button :type="img.visible ? 'error' : 'success'" @click="img.visible = !img.visible">
+					{{img.visible ? 'Hide' : 'Show'}}
+				</n-button>
+			</td>
+		</tr>
+	</tbody>
+</n-table>
+</template>
+
+<script setup lang="ts">
+import {NTable, NButton} from "naive-ui";
+
+import {replicant} from "../../browser-common/replicant";
+
+const images = await replicant('assets:images');
+</script>
+
+<style scoped lang="scss">
+	img {
+		width: 250px;
+		height: 140.6px;
+	}
+</style>
