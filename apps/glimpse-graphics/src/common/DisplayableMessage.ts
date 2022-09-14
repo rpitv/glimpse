1d18565fa3ac70f50df8df9428a29df71874f7d2
Erik Roberts <me@ecr.dev>
2022-09-14 15:15:34 -0400

    Add ability to add timers to messages (e.g. powerplay, play clock)

--- apps/glimpse-graphics/src/common/DisplayableMessage.ts
+++ apps/glimpse-graphics/src/common/DisplayableMessage.ts
@@ -1,9 +1,17 @@
+export type MessageTimer = {
+	visible: boolean;
+	startedAt: number;
+	length: number;
+}
+
 export class DisplayableMessage {
 	public readonly message: string;
 	public readonly type: "info"|"status";
+	public readonly timer: MessageTimer|null;
 
-	public constructor(message: string, type: "info"|"status") {
+	public constructor(message: string, type: "info"|"status", timer?: MessageTimer) {
 		this.message = message;
 		this.type = type;
+		this.timer = timer ?? null;
 	}
 }
