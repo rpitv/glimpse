b94e31fb63ba743beabd84df9f8cdea8c83caff3
Erik Roberts <me@ecr.dev>
2022-10-21 18:19:15 -0400

    Add process restarting so that if the ffmpeg command crashes for whatever reason, it auto restarts it for up to an hour. Tries every 1/10th of a second for the first 3 seconds, then every second for 2 minutes, then every 5 seconds for an hour.

--- apps/control/control/src/index.ts
+++ apps/control/control/src/index.ts
@@ -48,75 +48,13 @@ async function startStream(from: string, to: string): Promise<void> {
     });
     await channel.assertQueue(exclusiveQueueName, { exclusive: true });
 
-    /*
-ffmpeg -re -i <source> \
--c:v libx264 -preset veryfast -maxrate 6000k \
--bufsize 12000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 \
--ar 44100 -f flv -flvflags no_duration_filesize <destination>
- */
-    const proc = spawn("ffmpeg", [
-        "-re",
-        "-i",
-        from,
-        "-c:v",
-        "libx264",
-        "-preset",
-        "veryfast",
-        "-maxrate",
-        "6000k",
-        "-bufsize",
-        "12000k",
-        "-pix_fmt",
-        "yuv420p",
-        "-g",
-        "50",
-        "-c:a",
-        "aac",
-        "-b:a",
-        "160k",
-        "-ac",
-        "2",
-        "-ar",
-        "44100",
-        "-f",
-        "flv",
-        "-flvflags",
-        "no_duration_filesize",
-        to,
-    ]);
+    let procStopped = false;
+    let procRespawnTries = 0;
+    let procRespawnTriesResetTimeout: NodeJS.Timeout | null = null;
 
-    await channel.consume(exclusiveQueueName, async (rmqMessage) => {
-        if (!rmqMessage) {
-            return; // TODO throw error
-        }
-
-        const message = rmqMessage.content.toString();
-
-        if (process.env.DEBUG) {
-            console.log(`Received message ${message} for stream ${id}`);
-        }
-
-        if (message === "stop") {
-            channel.publish(
-                STATE_EXCHANGE_NAME,
-                "",
-                Buffer.from(
-                    JSON.stringify({
-                        id,
-                        to,
-                        from,
-                        message: "Stream stopping peacefully",
-                    })
-                )
-            );
-            proc.kill();
-        }
-    });
-
-    proc.stderr.on("data", (data) => {
-        if (process.env.DEBUG) {
-            console.log("[", id, "] ", data.toString());
-        }
+    // Continue to respawn the process until it's stopped manually or it's been attempted to respawn
+    //   for an hour with no success. (See below for logic behind 450)
+    while (!procStopped && procRespawnTries < 450) {
         channel.publish(
             STATE_EXCHANGE_NAME,
             "",
@@ -125,19 +63,153 @@ ffmpeg -re -i <source> \
                     id,
                     to,
                     from,
-                    message: data.toString(),
+                    message: `[try ${procRespawnTries}] Starting stream...`,
                 })
             )
         );
-    });
 
-    proc.on("close", async () => {
-        if (process.env.DEBUG) {
-            console.log("[" + id + "] Stream closed");
+        // Add a delay before trying to (re)start the stream
+        await new Promise((resolve) => {
+            // First try (or first retry), no delay.
+            if (procRespawnTries === 0) {
+                resolve(undefined);
+                return;
+            }
+            if (procRespawnTries < 29) {
+                // Second retry thru 3 seconds, 1/10th of a second delay.
+                setTimeout(resolve, 100);
+            } else if (procRespawnTries < 150) {
+                // 3 seconds thru 2 minutes, 1-second delay.
+                setTimeout(resolve, 1000);
+            } else {
+                // 2 minutes and beyond, 5-second delay.
+                setTimeout(resolve, 5000);
+            }
+        });
+        procRespawnTries++;
+
+        /*
+        ffmpeg -re -i <source> \
+        -c:v libx264 -preset veryfast -maxrate 6000k \
+        -bufsize 12000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 \
+        -ar 44100 -f flv -flvflags no_duration_filesize <destination>
+         */
+        const proc = spawn("ffmpeg", [
+            "-re",
+            "-i",
+            from,
+            "-c:v",
+            "libx264",
+            "-preset",
+            "veryfast",
+            "-maxrate",
+            "6000k",
+            "-bufsize",
+            "12000k",
+            "-pix_fmt",
+            "yuv420p",
+            "-g",
+            "50",
+            "-c:a",
+            "aac",
+            "-b:a",
+            "160k",
+            "-ac",
+            "2",
+            "-ar",
+            "44100",
+            "-f",
+            "flv",
+            "-flvflags",
+            "no_duration_filesize",
+            to,
+        ]);
+
+        // Setup and maintain timer to reset procRespawnTries if a stream is running for more than
+        //   10 seconds.
+        if (procRespawnTriesResetTimeout !== null) {
+            clearTimeout(procRespawnTriesResetTimeout);
         }
-        await channel.close();
-        await client.close();
-    });
+        procRespawnTriesResetTimeout = setTimeout(() => {
+            procRespawnTries = 0;
+        }, 10000);
+
+        // Incoming messages from users managing the stream
+        await channel.consume(exclusiveQueueName, async (rmqMessage) => {
+            if (!rmqMessage) {
+                return; // TODO throw error
+            }
+
+            const message = rmqMessage.content.toString();
+
+            if (process.env.DEBUG) {
+                console.log(`Received message ${message} for stream ${id}`);
+            }
+
+            if (message === "stop") {
+                procStopped = true;
+                channel.publish(
+                    STATE_EXCHANGE_NAME,
+                    "",
+                    Buffer.from(
+                        JSON.stringify({
+                            id,
+                            to,
+                            from,
+                            message: "Stream stopping peacefully",
+                        })
+                    )
+                );
+                proc.kill();
+            }
+        });
+
+        // Send out ffmpeg informational output to the users
+        proc.stderr.on("data", (data) => {
+            if (process.env.DEBUG) {
+                console.log("[", id, "] ", data.toString());
+            }
+            channel.publish(
+                STATE_EXCHANGE_NAME,
+                "",
+                Buffer.from(
+                    JSON.stringify({
+                        id,
+                        to,
+                        from,
+                        message: data.toString(),
+                    })
+                )
+            );
+        });
+
+        // Wait for this ffmpeg process to quit
+        await new Promise((resolve) => proc.on("close", resolve));
+    }
+
+    // We've exited the while loop, stream is actually stopped now and will not restart.
+    if (process.env.DEBUG) {
+        console.log("[" + id + "] Stream closed");
+    }
+
+    // Error message if stream wasn't manually stopped
+    if (!procStopped) {
+        channel.publish(
+            STATE_EXCHANGE_NAME,
+            "",
+            Buffer.from(
+                JSON.stringify({
+                    id,
+                    to,
+                    from,
+                    message: "Upstream source disconnected",
+                })
+            )
+        );
+    }
+
+    await channel.close();
+    await client.close();
 }
 
 async function start(): Promise<void> {
@@ -164,7 +236,7 @@ async function start(): Promise<void> {
                 return; // TODO throw error
             }
 
-            await startStream(message.from, message.to);
+            startStream(message.from, message.to);
             channel.ack(rmqMessage);
         },
         {
