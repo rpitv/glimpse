8337ed8c7c5d272f73a0a3b213766ba24ae87bbf
rpitv <rpitv-web@union.lists.rpi.edu>
2023-01-11 18:13:30 -0500

    Revert "Graphics now(?) logs SOGS"
    This reverts commit 73bd5f3a3fed1256c64dad598dedb330a3e2a4a4.

--- apps/glimpse-graphics/src/extension/daktronics-rtd/sports-definitions.ts
+++ apps/glimpse-graphics/src/extension/daktronics-rtd/sports-definitions.ts
@@ -24,7 +24,7 @@ while((match = regex.exec(str))) {
 console.log(matches);
  */
 
-import {awayScoreHandler, homeScoreHandler, mainClockHandler, periodHandler, awayShotHandler, homeShotHandler} from "./handlers";
+import {awayScoreHandler, homeScoreHandler, mainClockHandler, periodHandler} from "./handlers";
 
 type PacketDefinition = { length: number, title: string, justification: 'L'|'R', handler?: (value: string) => void };
 type SportDefinition = { [key: number]: PacketDefinition };
@@ -543,8 +543,7 @@ export const sports: { [key: string]: SportDefinition } = {
 		"422": {
 			"length": 3,
 			"justification": "R",
-			"title": "Home Shots On Goal - Total",
-			handler: homeShotHandler
+			"title": "Home Shots On Goal - Total"
 		},
 		"425": {
 			"length": 2,
@@ -654,8 +653,7 @@ export const sports: { [key: string]: SportDefinition } = {
 		"468": {
 			"length": 3,
 			"justification": "R",
-			"title": "Guest Shots On Goal - Total",
-			handler: awayShotHandler
+			"title": "Guest Shots On Goal - Total"
 		},
 		"471": {
 			"length": 2,
