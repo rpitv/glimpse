b6f88f015deec40ea929885b333f52277fb44ae4
1milibyte <83081917+1milibyte@users.noreply.github.com>
2023-01-16 00:27:17 -0500

    Added scoreboard
    Scoreboard is now automated minus having to automatically show it. No longer need to use photoshop for this.

---
+++ apps/glimpse-graphics/src/dashboard/lower-third/LowerThird.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h2>Display Scoreboard</h2>
+		<n-button @click="replicants.lowerThird.scoreboard.value = !replicants.lowerThird.scoreboard.value">
+			{{ replicants.lowerThird.scoreboard.value ? "Hide" : "Show" }}
+		</n-button>
+	</div>
+</template>
+
+<script setup lang="ts">
+import {NButton} from "naive-ui"
+import { loadReplicants } from "../../browser-common/replicants";
+
+const replicants = await loadReplicants();
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ apps/glimpse-graphics/src/dashboard/lower-third/main.vue
@@ -0,0 +1,22 @@
+<template>
+	<n-config-provider :theme="darkTheme">
+		<Suspense>
+			<div>
+				<LowerThird />
+			</div>
+			<template #fallback>
+				Loading...
+			</template>
+		</Suspense>
+	</n-config-provider>
+</template>
+
+<script setup lang="ts">
+import {NConfigProvider, darkTheme} from "naive-ui";
+import LowerThird from "./LowerThird.vue";
+
+</script>
+
+<style scoped>
+
+</style>
