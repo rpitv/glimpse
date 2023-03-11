bf461e953e6ee3f60c8394a6b8225ce04fbcf6d7
Erik <me@ecr.dev>
2023-03-11 17:38:11 -0500

    Refactor dashboard to use a side popout menu across all pages. Update naive-ui and cypress

---
+++ apps/glimpse-ui/src/components/dashboard/DashboardComingSoon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Coming Soon</h1>
+    <p>This feature is not yet available.</p>
+    <img src="https://i.imgur.com/u3InO3X.jpg" alt="RPI SIS Man 'Suffer'" />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+  div {
+    text-align: center;
+  }
+  img {
+    max-height: 300px;
+    border: 1px solid white;
+    border-radius: 5px;
+  }
+</style>
---
+++ apps/glimpse-ui/src/components/dashboard/DashboardRedirectsPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <DashboardComingSoon/>
+</template>
+
+<script setup lang="ts">
+import DashboardComingSoon from "@/components/dashboard/DashboardComingSoon.vue";
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
