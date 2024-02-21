341ac140b77374cd002218d4487fbe2001746422
Erik <7330807+robere2@users.noreply.github.com>
2024-02-21 09:33:22 -0500

    feat(ui): Update Dashboard to use Vuetify navigation

--- apps/ui/src/views/dashboard/DashboardView.vue
+++ apps/ui/src/views/dashboard/DashboardView.vue
@@ -1,87 +1,66 @@
 <template>
   <div class="dashboard-view">
-    <n-card>
-      <n-layout has-sider>
-        <n-layout-sider
-          bordered
-          collapse-mode="width"
-          :collapsed-width="64"
-          :width="240"
-          :collapsed="menuCollapsed"
-          show-trigger
-          @collapse="menuCollapsed = true"
-          @expand="menuCollapsed = false"
-          >
-            <n-menu
-              :options="menuOptions"
-              @update:value="menuOptionClicked"
+      <v-layout style="min-height: 680px">
+        <v-navigation-drawer absolute expand-on-hover rail>
+          <v-list density="compact" nav>
+            <v-list-item v-for="route of menuOptions.get(DashboardPageCategory.Content)"
+                         :title="route.title"
+                         :prepend-icon="route.icon"
+                         :value="route.route"
+                         @click="router.push({ name: route.route })"
             />
-        </n-layout-sider>
-        <n-layout class="page-content">
+            <v-divider />
+            <v-list-item v-for="route of menuOptions.get(DashboardPageCategory.Organization)"
+                         :title="route.title"
+                         :prepend-icon="route.icon"
+                         :value="route.route"
+                         @click="router.push({ name: route.route })"
+            />
+            <v-divider />
+            <v-list-item v-for="route of menuOptions.get(DashboardPageCategory.Admin)"
+                         :title="route.title"
+                         :prepend-icon="route.icon"
+                         :value="route.route"
+                         @click="router.push({ name: route.route })"
+            />
+          </v-list>
+        </v-navigation-drawer>
+
+        <v-card class="w-100 ml-13 pa-10 pt-5">
+        <v-card-title>
           <DashboardBreadcrumb :route="breadcrumbRoute"/>
+        </v-card-title>
+
+        <v-card-item>
           <router-view v-slot="{ Component }">
             <component :is="Component" />
           </router-view>
-        </n-layout>
-      </n-layout>
-    </n-card>
+        </v-card-item>
+
+        </v-card>
+      </v-layout>
   </div>
 </template>
 
 <script setup lang="ts">
-import { NCard, NMenu, NLayout, NLayoutSider } from "naive-ui";
-import type { MenuOption } from "naive-ui";
-import { computed, h, ref } from "vue";
-import { RouteRecordRaw, RouterLink, useRoute, useRouter } from "vue-router";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { DashboardPageCategory, DashboardPageMetadata } from "@/util";
+import {computed} from "vue";
+import {useRoute, useRouter} from "vue-router";
+import type { DashboardPageMetadata } from "@/util";
+import {DashboardPageCategory} from "@/util";
 import DashboardBreadcrumb from "@/components/dashboard/DashboardBreadcrumb.vue";
 
 const route = useRoute();
 const router = useRouter();
 
-const categoryOrder = [
-  DashboardPageCategory.Content,
-  DashboardPageCategory.Organization,
-  DashboardPageCategory.Admin
-];
+type MenuOption = { icon: string, title: string, route: string | symbol }
+const menuOptions = computed<Map<DashboardPageCategory, MenuOption[]>>(() => {
+  const childrenRoutes = router.getRoutes().find(r => r.name === "dashboard-parent")?.children ?? [];
 
-function generateOptionComponent(route: RouteRecordRaw) {
-  const routeMeta = route.meta?.sider as DashboardPageMetadata;
-
-  let toRouteName = route.name;
-  if(route.children && route.children?.length > 0) {
-    const emptyPathChild = route.children.find(c => c.path === "");
-    if(emptyPathChild) {
-      toRouteName = emptyPathChild.name;
-    }
-  }
-
-  return {
-    label: () => {
-      return h(
-        RouterLink,
-        { to: { name: toRouteName } },
-        {
-          default: () => routeMeta.title ?? toRouteName
-        }
-      )
-    },
-    key: `${String(toRouteName)}`,
-    icon: () => {
-      return h(FontAwesomeIcon, { icon: routeMeta.icon })
-    }
-  }
-}
-
-const menuOptions = computed<MenuOption[]>(() => {
-  const childrenRoutes = router.getRoutes().find(r => r.name === "dashboard-parent")?.children;
-  if(!childrenRoutes) {
-    return [];
-  }
-
-  const categories: Map<DashboardPageCategory, MenuOption[]> = new Map();
-
+  const groupedRoutesByCategory: Map<DashboardPageCategory, MenuOption[]> = new Map([
+    [DashboardPageCategory.Content, []],
+    [DashboardPageCategory.Organization, []],
+    [DashboardPageCategory.Admin, []]
+  ]);
   for(const route of childrenRoutes) {
     const routeMeta: DashboardPageMetadata = route.meta?.sider as any ?? {};
 
@@ -89,34 +68,27 @@ const menuOptions = computed<MenuOption[]>(() => {
       continue;
     }
 
-    const category = routeMeta.category ?? DashboardPageCategory.Organization;
-    const categoryEntries = categories.get(category);
-    if(categoryEntries) {
-      categoryEntries.push(generateOptionComponent(route));
-    } else {
-      categories.set(category, [generateOptionComponent(route)]);
-    }
-  }
-
-  const options: MenuOption[] = [];
-
-  for(let i = 0; i < categoryOrder.length; i++) {
-    const categoryName = categoryOrder[i];
-    const nextCategoryName = categoryOrder[i + 1];
-    const categoryOptions = categories.get(categoryName);
-    if(categoryOptions) {
-      options.push(...categoryOptions);
-
-      if(categoryOptions.length && categories.get(nextCategoryName)?.length ) {
-        options.push({
-          key: `divider:${categoryName}:${nextCategoryName}`,
-          type: 'divider'
-        })
+    let routeName = route.name;
+    if(route.children && route.children?.length > 0) {
+      const emptyPathChild = route.children.find(c => c.path === "");
+      if(emptyPathChild) {
+        routeName = emptyPathChild.name;
       }
     }
+
+    if(!routeName) {
+      throw new Error(`Dashboard navigation bar requires that all routes have a name, but one was missing from ${route}`)
+    }
+
+    const category = routeMeta.category ?? DashboardPageCategory.Organization;
+    groupedRoutesByCategory.get(category)!.push({
+      icon: routeMeta.icon ?? 'fas fa-question-mark',
+      title: routeMeta.title ?? "Unknown",
+      route: routeName
+    });
   }
 
-  return options;
+  return groupedRoutesByCategory
 });
 
 const breadcrumbRoute = computed(() => {
@@ -127,19 +99,6 @@ const breadcrumbRoute = computed(() => {
     return breadcrumb;
   }
 })
-
-function menuOptionClicked(key: string) {
-  if(key.includes("divider")) {
-    return;
-  }
-  const route = key.split(':').pop();
-  if(!route) {
-    return;
-  }
-  router.push({ name: key });
-}
-
-const menuCollapsed = ref<boolean>(true);
 </script>
 
 <style lang="scss">
