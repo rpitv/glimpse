0b3a309c3a2d4e3738f536c7c32b7d5d660c8769
Erik Roberts <7330807+robere2@users.noreply.github.com>
2024-12-07 17:40:01 -0500

    wip(rpc): Refactor + compile.ts script

--- packages/rpc/scripts/compile.ts
+++ packages/rpc/scripts/compile.ts
@@ -2,21 +2,28 @@ import * as os from 'os';
 import * as child_process from "child_process"
 import * as fs from 'fs';
 
-fs.mkdirSync(__dirname + '/dist', {recursive: true})
+fs.rmSync('dist', { recursive: true, force: true })
+fs.mkdirSync('dist', {recursive: true})
 
-let pluginOption = "";
+let pluginOption: string;
 if(os.platform() === "win32") {
-    pluginOption = "protoc-gen-ts_proto=\"..\\..\\node_modules\\.bin\\protoc-gen-ts_proto.cmd\""
+    pluginOption = "protoc-gen-ts_proto=\"..\\\\..\\\\node_modules\\\\.bin\\\\protoc-gen-ts_proto.cmd\""
 } else {
     pluginOption = "../../node_modules/.bin/protoc-gen-ts_proto"
 }
 
-const cmd = `protoc --plugin=${pluginOption} --proto_path=./src --ts_proto_out=. ./src/service.proto`
+const cmd = `protoc --plugin=${pluginOption} --proto_path=./src/protos --ts_proto_out=./dist ./src/protos/*.proto`
 
-const proc = child_process.spawn(cmd)
-proc.stdout.pipe(process.stdout)
-proc.stderr.pipe(process.stderr)
-
+const proc = child_process.exec(cmd)
+proc.stdout?.on('data', (data: Buffer) => {
+    console.log(data.toString())
+})
+proc.stderr?.on('data', (data: Buffer) => {
+    console.error(data.toString())
+})
 proc.on('close', (code: number) => {
     console.log(`Process exited with code ${code}`)
 })
+
+fs.cpSync('./LICENSE', './dist/LICENSE')
+fs.cpSync('./src/protos', './dist/protos/', {recursive: true})
